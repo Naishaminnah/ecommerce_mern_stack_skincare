b7b70295a390cce39d378438ec9182ed62fb4612
Naishaminnah <155439876+Naishaminnah@users.noreply.github.com>
2024-05-08 17:09:16 +0530

    Add files via upload

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+    
+  return (
+    <div className='descriptionbox'>
+
+    <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+    </div>
+    <div className="descriptionbox-description">
+     <p>Manufactured from non-artificial and natural materials for betterment of your wellness.
+      We assure you that it will not cause any damage to your body.   </p>
+     <p>Authentic product manufactured from one of the best and popular wellness brands. We gurantee that none of our products will be harmful for any kind of skin,hair. These are curated from the best and by the best in the indutry.</p>
+     </div>
+
+    </div>
+  )
+}
+export default DescriptionBox
