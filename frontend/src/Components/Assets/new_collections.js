888f7b3c4547a91791475fe5ba1f5b0773b27234
Naishaminnah <155439876+Naishaminnah@users.noreply.github.com>
2024-05-08 16:54:29 +0530

    Add files via upload

---
+++ frontend/src/Components/Assets/new_collections.js
@@ -0,0 +1,79 @@
+import p2_img from './product_2.avif'
+import p12_img from './product_12.avif'
+import p17_img from './product_17.avif'
+import p28_img from './product_28.avif'
+import p15_img from './product_15.avif'
+import p14_img from './product_14.avif'
+import p35_img from './product_35.avif'
+import p8_img from './product_8.avif'
+
+
+
+
+let new_collections = [
+  {
+    id: 2,
+    name: "Aroma Magic Sunblock Lotion SPF 30 PA++",
+    category: "Skincare",
+    image: p2_img,
+    new_price: 850.0,
+    old_price: 1200.5,
+  },
+  {
+    id: 12,
+    name: "CeraVe Foaming Cleanser Face Wash For Oily Skin With Hyaluronic Acid, Ceramides & Niacinamide",
+    category: "Skincare",
+    image: p12_img,
+    new_price: 385.0,
+    old_price: 620.5,
+  },
+  {
+    id: 17,
+    name: "Minimalist 2% Salicylic Acid + LHA Face Cleanser With Zinc For Reducing Sebum & Acne",
+    category: "Skincare",
+    image: p17_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 28,
+    name: "Estee Lauder Advanced Night Repair Synchronized Multi-Recovery Complex Serum With Tripeptide-32 20ml",
+    category: "Skincare",
+    image: p28_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 15,
+    name: "Forest Essentials Delicate Facial Cleanser with Saffron & Neem - Ayurvedic Face Wash For Oily Skin",
+    category: "Skincare",
+    image: p15_img,
+    new_price: 80.0,
+    old_price: 140.5,
+  },
+  {
+    id: 14,
+    name: "Clinique Take The Day Off Cleansing Balm (Makeup Remover)",
+    category: "Skincare",
+    image: p14_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 35,
+    name: "Huda Beauty Faux Filter Color Corrector - Papaya",
+    category: "Makeup",
+    image: p35_img,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 8,
+    name: "Minimalist SPF 50 PA ++++ Sunscreen With Multi-Vitamin For Reducing Photoaging & No White Cast",
+    category: "Skincare",
+    image: p8_img,
+    new_price: 345.0,
+    old_price: 370.5,
+  },
+]
+export default new_collections;
