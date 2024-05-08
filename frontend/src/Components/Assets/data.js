7425e20c55fc5d6d9a2dbb6a85c3a09f91e0f0fd
Naishaminnah <155439876+Naishaminnah@users.noreply.github.com>
2024-05-08 16:53:07 +0530

    Add files via upload

---
+++ frontend/src/Components/Assets/data.js
@@ -0,0 +1,41 @@
+import p1_img from './product_1.avif'
+import p2_img from './product_2.avif'
+import p3_img from './product_3.avif'
+import p4_img from './product_4.avif'
+
+let data_product = [
+  {
+    id: 1,
+    name: "Aqualogica Glow+ Dewy Sunscreen with Papaya & Vitamin C",
+    category: "Skincare",
+    image: p1_img,
+    new_price: 250.0,
+    old_price: 450.5,
+  },
+  {
+    id: 2,
+    name: "Aroma Magic Sunblock Lotion SPF 30 PA++",
+    category: "Skincare",
+    image: p2_img,
+    new_price: 850.0,
+    old_price: 1200.5,
+  },
+  {
+    id: 3,
+    name: "Dot & Key Strawberry On-The-Go SPF 50 Sunscreen Stick",
+    category: "Skincare",
+    image: p3_img,
+    new_price: 70.0,
+    old_price: 100.5,
+  },
+  {
+    id: 4,
+    name: "Lakme Sun Expert SPF 50 PA+++ Tinted Sunscreen with Natural Matte Finish",
+    category: "Skincare",
+    image: p4_img,
+    new_price: 100.0,
+    old_price: 150.0,
+  },
+];
+
+export default data_product;
